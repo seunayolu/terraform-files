cdcf39e95128a4f3cf76dbb61e46bbb8d8196924
seunayolu <seunayolu@gmail.com>
2023-09-13 14:45:35 -0500

    reverted_userdata

--- modules/webserver/outputs.tf
+++ modules/webserver/outputs.tf
@@ -2,6 +2,6 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image.id
 }
 
-output "ec2_instance_public_ip" {
+output "instance" {
   value = aws_instance.class-ec2-server.public_ip
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "ec2_instance_public_ip" {
-  value = module.class-ec2-server.ec2_instance_public_ip.public_ip
+  value = module.class-ec2-server.instance.public_ip
 }
