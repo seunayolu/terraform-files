fe09404a2742abc15ff99536047e1172a47d66af
seunayolu <seunayolu@gmail.com>
2023-12-18 13:53:52 -0600

    aws_region_updated

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket         	   = "workspace-terrabackend"
     key                = "state/terraform.tfstate"
-    region         	   = "us-east-2"
+    region         	   = "eu-west-2"
   }
 }
 
