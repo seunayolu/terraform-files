0c9a14bd9cdc9bd9b865fb2a9aee98e28c272053
seunayolu <seunayolu@gmail.com>
2024-05-27 02:38:45 +0100

    source.tf updated

--- source.tf
+++ source.tf
@@ -125,7 +125,7 @@ data "aws_ami" "master-class-ami" {
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.3.*"]
+    values = ["al2023-ami-2023.*.*"]
   }
 
   filter {
